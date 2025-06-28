0dab9b9f003d5354e2af2227464d5962c740f74f
usertam <code@usertam.dev>
2025-06-29 00:22:17 +0800

    users.cc: mute home is not owned by you

--- src/libutil/unix/users.cc
+++ src/libutil/unix/users.cc
@@ -52,7 +52,7 @@ Path getHome()
         if (!homeDir) {
             homeDir = getHomeOf(geteuid());
             if (unownedUserHomeDir.has_value() && unownedUserHomeDir != homeDir) {
-                warn(
+                debug(
                     "$HOME ('%s') is not owned by you, falling back to the one defined in the 'passwd' file ('%s')",
                     *unownedUserHomeDir,
                     *homeDir);
