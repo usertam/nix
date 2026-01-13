1de8514b4949255f7d9a33f4606ed27ac0282ecc
usertam <code@usertam.dev>
2026-01-14 03:29:07 +0800

    Reduce verbosity and allow NIXPKGS_* environment variables
    - Allow NIXPKGS_ALLOW_* environment variables without --impure flag
    - Convert warnings to debug messages for missing search paths
    - Remove interactive prompts for untrusted flake configuration
    - Ignore cached eval errors and continue evaluation
    - Mute home directory ownership warnings

--- src/libflake/config.cc
+++ src/libflake/config.cc
@@ -87,30 +87,9 @@ void ConfigFile::apply(const Settings & flakeSettings)
                 trusted = *saved;
                 printInfo(
                     "Using saved setting for '%s = %s' from ~/.local/share/nix/trusted-settings.json.", name, valueS);
-            } else {
-                // FIXME: filter ANSI escapes, newlines, \r, etc.
-                if (std::tolower(logger
-                                     ->ask(
-                                         fmt("do you want to allow configuration setting '%s' to be set to '" ANSI_RED
-                                             "%s" ANSI_NORMAL "' (y/N)?",
-                                             name,
-                                             valueS))
-                                     .value_or('n'))
-                    == 'y') {
-                    trusted = true;
-                }
-                if (std::tolower(logger
-                                     ->ask(
-                                         fmt("do you want to permanently mark this value as %s (y/N)?",
-                                             trusted ? "trusted" : "untrusted"))
-                                     .value_or('n'))
-                    == 'y') {
-                    trustedList[name][valueS] = trusted;
-                    writeTrustedList(trustedList);
-                }
             }
             if (!trusted) {
-                warn(
+                debug(
                     "ignoring untrusted flake configuration setting '%s'.\nPass '%s' to trust it",
                     name,
                     "--accept-flake-config");
--- src/libutil/unix/users.cc
+++ src/libutil/unix/users.cc
@@ -52,7 +52,7 @@ std::filesystem::path getHome()
         if (!homeDir) {
             homeDir = getHomeOf(geteuid());
             if (unownedUserHomeDir.has_value() && unownedUserHomeDir != homeDir) {
-                warn(
+                debug(
                     "$HOME ('%s') is not owned by you, falling back to the one defined in the 'passwd' file ('%s')",
                     *unownedUserHomeDir,
                     *homeDir);
